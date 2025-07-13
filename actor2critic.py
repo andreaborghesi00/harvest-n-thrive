9bb6c3d6f1d7711ad21fe23382e5d38711a60d09
andreaborghesi00 <00borghesiandrea@gmail.com>
2025-07-13 10:57:10 +0200

    updated actor critic, it's still meh

--- actor2critic.py
+++ actor2critic.py
@@ -1,11 +1,8 @@
-import gymnasium as gym
-import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import torch.nn.functional as F
-import cv2
 from torch.distributions import Normal, Categorical, Bernoulli
+from icm import ICM
 
 EXPLORATION_COEFFICIENT = 0.2
 EXPLORATION_COEFFICIENT_DECAY = 0.99
@@ -24,9 +21,11 @@ class ResidualBlock(nn.Module):
         return x + self.block(x)
     
 class HybridActorCritic(nn.Module):
-    def __init__(self, input_dim, total_cells):
+    def __init__(self, input_dim, total_cells, n_crops):
         super().__init__()
         self.total_cells = total_cells
+        self.n_crops = n_crops
+        
         self.shared =nn.Sequential(
             nn.Linear(input_dim, 256),
             nn.LayerNorm(256),
@@ -50,24 +49,48 @@ class HybridActorCritic(nn.Module):
             nn.Linear(512, 256),
             nn.LayerNorm(256),
             nn.SiLU(inplace=True),
-            ResidualBlock(256),
+
             nn.Linear(256, 128),
             nn.LayerNorm(128),
             nn.SiLU(inplace=True),
+
             nn.Linear(128, 64),
             nn.LayerNorm(64),
             nn.SiLU(inplace=True),
+
             nn.Linear(64, 1)
             
         )
         
         # policy heads
-        self.water_head      = nn.Linear(256, total_cells)
-        self.fertilizer_head = nn.Linear(256, total_cells)
-        self.water_log_std   = nn.Parameter(torch.zeros(total_cells))
+        # Continuous heads
+        # self.water_head = nn.Linear(256, total_cells)
+        self.water_head = nn.Sequential(
+            nn.Linear(256, total_cells)
+        )
+        self.fertilizer_head = nn.Sequential(
+            nn.Linear(256, total_cells)
+        )
+        
+        self.water_log_std = nn.Parameter(torch.zeros(total_cells))
         self.fertilizer_log_std = nn.Parameter(torch.zeros(total_cells))
-        self.crop_mask_head   = nn.Linear(256, total_cells)
-        self.crop_select_head = nn.Linear(256, 4)
+        
+        # Discrete heads
+        # self.crop_mask_head = nn.Sequential(
+        #     nn.Linear(256, total_cells  * (self.n_crops + 1)) 
+        #     ) # 4 crop types + 1 for no crop selected, we multiply to one-hot encode the crop mask
+        
+        self.crop_mask_head = nn.Sequential(
+            nn.Linear(256, total_cells)
+        )        
+        
+        self.crop_type_head = nn.Sequential(
+            nn.Linear(256, self.n_crops)
+        )
+        
+        self.harvest_mask_head = nn.Sequential(
+            nn.Linear(256, total_cells)
+            )  # binary harvest mask, 1 if harvest, 0 if not
         
         # value head
         # self.value_head = nn.Linear(256, 1)
@@ -78,65 +101,82 @@ class HybridActorCritic(nn.Module):
         policy_features = self.policy_head(h)  # shared features for policy heads
         
         # policy dists
-        mu_water = self.water_head(policy_features)
-        std_water = self.water_log_std.exp().unsqueeze(0)
-        dist_water = Normal(mu_water, std_water)
+        water_mean = torch.sigmoid(self.water_head(policy_features))
+        water_std = torch.exp(self.water_log_std)
 
-        mu_fertilizer = self.fertilizer_head(policy_features)
-        std_fertilizer = self.fertilizer_log_std.exp().unsqueeze(0)
-        dist_fertilizer = Normal(mu_fertilizer, std_fertilizer)
+        fertilizer_mean = torch.sigmoid(self.fertilizer_head(policy_features))
+        fertilizer_std = torch.exp(self.fertilizer_log_std)
         
-        crop_mask_logits = self.crop_mask_head(policy_features)
-        dist_crop_mask = Bernoulli(logits=crop_mask_logits)
-        
-        crop_select_logits = self.crop_select_head(policy_features)
-        dist_crop_sel = Categorical(logits=crop_select_logits)
+        crop_mask_logits = self.crop_mask_head(policy_features) # take the logits
+        harvest_mask_logits = self.harvest_mask_head(policy_features)  # binary logits for harvest mask
+        crop_select_logits = self.crop_type_head(policy_features) # also take the logits
         
         v = self.value_head(h).squeeze(-1)     # feed the "decisions" into the critic head
-        return dist_water, dist_fertilizer, dist_crop_sel, dist_crop_mask, v
+        return water_mean, water_std, fertilizer_mean, fertilizer_std, crop_mask_logits, harvest_mask_logits, crop_select_logits, v
 
 
 class ActorCriticAgent:
-    def __init__(self, state_dim, total_cells, learning_rate: float = 3e-4, gamma: float = .99, c1: float = 0.5, device=torch.device("cuda" if torch.cuda.is_available() else "cpu")):
+    def __init__(self,
+                 state_dim, 
+                 action_dim,
+                 total_cells, 
+                 n_crops=4,
+                 learning_rate: float = 3e-4, 
+                 gamma: float = .99, 
+                 c1: float = 0.5,
+                 use_icm: bool = False,
+                 device=torch.device("cuda" if torch.cuda.is_available() else "cpu")):
         self.device = device
-        self.a2c = HybridActorCritic(state_dim, total_cells).to(self.device)
+        self.a2c = HybridActorCritic(state_dim, total_cells, n_crops).to(self.device)
         self.optimizer = optim.Adam(self.a2c.parameters(), lr=learning_rate)
         self.scheduler = optim.lr_scheduler.ReduceLROnPlateau(self.optimizer, mode='max', factor=0.5, patience=20, verbose=True)
         self.memory = []  # Stores (log_prob, reward, state)
         self.gamma = gamma
         self.c1 = c1  # Coefficient for critic loss
+        icm = ICM(state_dim=state_dim, action_dim=action_dim, device=self.device).to(self.device) if use_icm else None
+        
     def select_action_hybrid(self, state):
         state = torch.FloatTensor(state).unsqueeze(0).to(self.device)
-        water_dist, fertilizer_dist, crop_select_dist, crop_mask_dist, v = self.a2c(state)
+        water_mean, water_std, fertilizer_mean, fertilizer_std, crop_mask_logits, harvest_mask_logits, crop_select_logits, v = self.a2c(state)
         
         # Sample continuous actions from Gaussian policy
+        water_dist = torch.distributions.Normal(water_mean, water_std)
         water_action = water_dist.sample()
         water_log_prob = water_dist.log_prob(water_action).sum(dim=-1)  # Compute log probability
                 
+        fertilizer_dist = torch.distributions.Normal(fertilizer_mean, fertilizer_std)
         fertilizer_action = fertilizer_dist.sample()
         fertilizer_log_prob = fertilizer_dist.log_prob(fertilizer_action).sum(dim=-1)  # Compute log probability
         
         # Sample discrete actions
+        crop_mask_dist = torch.distributions.Bernoulli(logits=crop_mask_logits)
         crop_mask_action = crop_mask_dist.sample()
         crop_mask_log_prob = crop_mask_dist.log_prob(crop_mask_action).sum(dim=-1)  # Compute log probability
         
+        crop_select_dist = torch.distributions.Categorical(logits=crop_select_logits)
         crop_select_action = crop_select_dist.sample()
         crop_select_log_prob = crop_select_dist.log_prob(crop_select_action)  # Compute log probability
         
+        harvest_mask_dist = torch.distributions.Bernoulli(logits=harvest_mask_logits)
+        harvest_mask_action = harvest_mask_dist.sample()
+        harvest_mask_log_prob = harvest_mask_dist.log_prob(harvest_mask_action).sum(dim=-1)  # Compute log probability
+
         # Combine actions and log probabilities
         action = {
             'water_amount': torch.clamp(water_action, 0, 1).detach().cpu().numpy().squeeze(),
             'fertilizer_amount': torch.clamp(fertilizer_action, 0, 1).detach().cpu().numpy().squeeze(),
             'crop_mask': crop_mask_action.squeeze(0).detach().cpu().numpy(),
-            'crop_selection': crop_select_action.item()
+            'harvest_mask': harvest_mask_action.squeeze(0).detach().cpu().numpy(),
+            'crop_type': crop_select_action.item()
         }
-        log_prob = water_log_prob + fertilizer_log_prob + crop_mask_log_prob + crop_select_log_prob
+        log_prob = water_log_prob + fertilizer_log_prob + crop_mask_log_prob + harvest_mask_log_prob + crop_select_log_prob
         entropy = (
             water_dist.entropy().mean()
             + fertilizer_dist.entropy().mean()
             + crop_mask_dist.entropy().mean()
-            + crop_select_dist.entropy().mean()
-        ) / 4
+            + harvest_mask_dist.entropy().mean()
+            + crop_select_dist.entropy()
+        ) / 5
         return action, log_prob, entropy, v
         
     def store_outcome(self, log_prob, reward, v, next_state, entropy):
@@ -176,15 +216,15 @@ class ActorCriticAgent:
         
         self.optimizer.zero_grad()
         loss.backward()
-        torch.nn.utils.clip_grad_norm_(self.a2c.parameters(), max_norm=0.5)
+        torch.nn.utils.clip_grad_norm_(self.a2c.parameters(), max_norm=1.0)
         self.optimizer.step()
         
         episode_reward = sum(e['reward'] for e in self.memory)
         self.scheduler.step(episode_reward)
         
         # Exploration coefficient decay
-        EXPLORATION_COEFFICIENT *= EXPLORATION_COEFFICIENT_DECAY
-        EXPLORATION_COEFFICIENT = max(EXPLORATION_COEFFICIENT, 0.01)
+        # EXPLORATION_COEFFICIENT *= EXPLORATION_COEFFICIENT_DECAY
+        # EXPLORATION_COEFFICIENT = max(EXPLORATION_COEFFICIENT, 0.01)
         
         # Clear memory after updating policy
-        self.memory = []
+        self.memory.clear()
--- train_a2c.py
+++ train_a2c.py
@@ -1,61 +1,52 @@
 import environment as env
-from reinforce import PolicyGradientAgent
 import numpy as np
 import gymnasium as gym
 from tqdm import tqdm
-import matplotlib.pyplot as plt
-import random
-import torch
-from actor2critic import HybridActorCritic, ActorCriticAgent
 import signal
 import threading
+from pathlib import Path
+from config import get_config
+from actor2critic import ActorCriticAgent
 
+CONFIGS = get_config()
 
-LEARNING_RATE = 3e-4
-GAMMA = 0.99  # Discount factor
-EPISODES = 1000  # Number of training episodes
-BATCH_SIZE = 10  # Update policy after X episodes
-HIDDEN_UNITS = 256  # Reduced hidden layer size for efficiency
-C1 = 0.5  # Coefficient for critic loss
-YEARS = 10
-DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+Path(CONFIGS.rewards_path).mkdir(parents=True, exist_ok=True)
+Path(CONFIGS.infos_path).mkdir(parents=True, exist_ok=True)
+Path(CONFIGS.configs_path).mkdir(parents=True, exist_ok=True)
 
 shutdown_flag = threading.Event()
+sigusr1_flag = threading.Event()
 
 def on_sigint(signum, frame):
     print("\nSIGINT received; will stop after this episode.")
     shutdown_flag.set()
+
+def on_sigusr1(signum, frame):
+    print("\nSIGUSR1 received; will save current state.")
+    sigusr1_flag.set()
     
 signal.signal(signal.SIGINT, on_sigint)
+signal.signal(signal.SIGUSR1, on_sigusr1)
+
 
 def flatten_observation(obs):
     return np.concatenate([
         obs['farm_state'].flatten(),
         obs['water_supply'],
         obs['fertilizer_supply'],
-        obs['labor_supply'],
+        obs['labour_supply'],
         [obs['current_week'] / 52],
-        [obs['current_year'] / YEARS]
+        [obs['current_year'] / CONFIGS.years]
     ])
-    
     
 def flatten_action(action):
     return np.concatenate([
         action['crop_mask'].flatten(),
-        [action['crop_selection']],
-        # action['harvest_mask'].flatten(),
+        [action['crop_type']],
+        action['harvest_mask'].flatten(),
         action['water_amount'].flatten(),
         action['fertilizer_amount'].flatten()
     ])
-
-
-def agent_action_to_env(action, total_cells):
-    return {
-        'crop_mask': (action[:total_cells] > 0).astype(int),  # Threshold at 0
-        'crop_selection': np.clip(np.round(action[2*total_cells]), 0, 3).astype(int),
-        'water_amount': (action[total_cells:2*total_cells] + 1) / 2,  # Scale to [0,1]
-        'fertilizer_amount': (action[2*total_cells:3*total_cells] + 1) / 2
-    }
 
 
 def average_infos(infos):
@@ -64,121 +55,135 @@ def average_infos(infos):
         avg_info[key] = np.mean([info[key] for info in infos])
     return avg_info
 
+
 def random_train(farm_env, episodes):
     pbar = tqdm(total=episodes, desc="Training", unit="step")
     mean_rewards = []
-    water_reserve = []
-    fertilizer_reserve = []
-    state, _ = farm_env.reset()
+    farm_env.reset()
     infos = []
     for episode in range(episodes):
-        obs, _ = farm_env.reset()
-        state = flatten_observation(obs)
+        if shutdown_flag.is_set():
+            break
+        
+        farm_env.reset()
         terminated = False
         truncated = False
         rewards = []
         
         while not (terminated or truncated):
             action = farm_env.action_space.sample()
-            # action, log_prob = agent.select_action_hybrid(state)
-            # next_state, reward, terminated, truncated, info = farm_env.step(agent_action_to_env(action=action, total_cells=total_cells))
-            next_state, reward, terminated, truncated, info = farm_env.step(action)
-            # agent.store_outcome(log_prob, reward, state)
-            state = flatten_observation(next_state)
+            _, reward, terminated, truncated, info = farm_env.step(action)
             rewards.append(reward)
         infos.append(info)
         pbar.update(1)
-        pbar.set_postfix(reward=np.mean(rewards))
+        pbar.set_postfix({
+            "reward":np.mean(rewards),
+            "harvest_reward": info['harvest_reward'],
+                          })
         mean_rewards.append(np.mean(rewards))
         
     pbar.close()
     print(average_infos(infos))
-    print(mean_rewards)
-    np.save('random_mean_rewards.npy', mean_rewards)
-    plt.figure(figsize=(12, 6))
-    plt.plot(mean_rewards)
-    plt.xlabel('Episodes')
-    plt.ylabel('Mean Reward')
-    plt.title('Random mean rewards')
-    plt.grid()
-    plt.ylim(-3, 5)
-    plt.savefig('random_mean_rewards.png')
-
-
-def main():
-    farm_size = (4, 4)
-    total_cells = farm_size[0] * farm_size[1]
-    farm_env = gym.envs.make("Farm-v0", years=YEARS, farm_size=farm_size, yearly_water_supply=9*52, yearly_fertilizer_supply=5*52, yearly_labor_supply=1000)
+    np.save(CONFIGS.rewards_path / f'random_{CONFIGS.experiment_name}.npy', mean_rewards)
+    np.save(CONFIGS.infos_path / f'random_{CONFIGS.experiment_name}_infos.npy', infos)
     
+def main():
+    farm_size = (CONFIGS.farm_size, CONFIGS.farm_size)
+    weekly_water_supply = farm_size[0] * farm_size[1] * CONFIGS.weekly_water_tile_supply  
+    weekly_fertilizer_supply = farm_size[0] * farm_size[1] * CONFIGS.weekly_fertilizer_tile_supply  
+    weekly_labour_supply = farm_size[0] * farm_size[1] * CONFIGS.weekly_labour_tile_supply
+    farm_env = gym.envs.make("Farm-v0", 
+                             years=CONFIGS.years, 
+                             farm_size=farm_size, 
+                             yearly_water_supply=weekly_water_supply*52, 
+                             yearly_fertilizer_supply=weekly_fertilizer_supply*52, 
+                             weekly_labour_supply=weekly_labour_supply,
+                             exp=CONFIGS.yield_exponent)
+    
+    # save a json with the configs used in this training:
+    
+
     sample_obs, _ = farm_env.reset()
     sample_action = farm_env.action_space.sample()
     
-    state_dim = flatten_observation(sample_obs).shape[0] # total_cells * 6 + 5
+    total_cells = farm_size[0] * farm_size[1]
+    state_dim = flatten_observation(sample_obs).shape[0] 
+    action_dim = flatten_action(sample_action).shape[0] 
     
     print("Predicted Observation Space Shape:", (state_dim,))
     print("Observation Space Shape:", flatten_observation(sample_obs).shape)
     print("Sample Action Shape:", flatten_action(sample_action).shape)
  
-    random_train(farm_env, 50)
-
-    agent = ActorCriticAgent(state_dim=state_dim, total_cells=total_cells, learning_rate=LEARNING_RATE, gamma=GAMMA, c1=C1, device=DEVICE)
+    # random_train(farm_env, 50)
     
-    pbar = tqdm(total=EPISODES, desc="Training", unit="step")
+    agent = ActorCriticAgent(state_dim=state_dim,
+                             action_dim=action_dim,
+                             total_cells=total_cells, 
+                             learning_rate=CONFIGS.learning_rate, 
+                             gamma=CONFIGS.gamma, 
+                            #  c1=CONFIGS.c1, # default 0.5
+                             device=CONFIGS.device)
+    
+    epsilon = CONFIGS.eps_start
+    
+    pbar = tqdm(total=CONFIGS.episodes, desc="Training", unit="step")
     mean_rewards = []
-    state, _ = farm_env.reset()
     infos = []
-    # main loop
-    for episode in range(EPISODES):
+    state, _ = farm_env.reset()
+    for episode in range(CONFIGS.episodes):
+        r_ints = []
+        r_exts = []
         if shutdown_flag.is_set():
             break
-
-        obs, _ = farm_env.reset()
+        
+        if sigusr1_flag.is_set():
+            np.save(CONFIGS.infos_path / f'a2c_{CONFIGS.experiment_name}_infos.npy', infos)
+            print("saved. resuming training...")
+            sigusr1_flag.clear()
+        
+        obs, _ = farm_env.reset(options={'episode': episode})
         state = flatten_observation(obs)
         terminated = False
         truncated = False
         rewards = []
-        
-        # single-episode loop
+        agent.episode = episode  # Update the current episode for beta cycle
+        r_ints = []
+        r_exts = [] 
         while not (terminated or truncated):
             action, log_prob, entropy, v = agent.select_action_hybrid(state)
                 
             next_state, reward, terminated, truncated, info = farm_env.step(action)
-            agent.store_outcome(log_prob=log_prob, reward=reward, next_state=flatten_observation(next_state) if not (terminated or truncated) else None, v=v, entropy=entropy)
+            agent.store_outcome(log_prob=log_prob,
+                                reward=reward, 
+                                next_state=flatten_observation(next_state) if not (terminated or truncated) else None, 
+                                v=v, 
+                                entropy=entropy)
             state = flatten_observation(next_state)
             
             rewards.append(reward)
         
         pbar.update(1)
-        pbar.set_postfix(reward=np.mean(rewards))
+        pbar.set_postfix({
+            "tot_reward": np.mean(rewards),
+            # "int": np.mean(r_ints) if len(r_ints) > 0 else 0.0,
+            # "ext": np.mean(r_exts),
+            "harvest": info['harvest_reward'],
+        })        
         mean_rewards.append(np.mean(rewards))
         infos.append(info)
-        # update policy every BATCH_SIZE episodes
-        if (episode + 1) % BATCH_SIZE == 0:
-            agent.update()
-            print(average_infos(infos[-BATCH_SIZE:]))
-
         
-        if episode % 10 == 0:
-            print(f"Action stats: "
-                f"Water={action['water_amount'].mean():.2f}±{action['water_amount'].std():.2f} "
-                f"Fert={action['fertilizer_amount'].mean():.2f} "
-                f"CropMask={action['crop_mask'].mean():.2f} "
-                f"CropSel={action['crop_selection']}")
+        # update policy every BATCH_SIZE episodes
+        if (episode + 1) % CONFIGS.batch_size == 0:
+            agent.update()
+            print(average_infos(infos[-CONFIGS.batch_size:]))
             
     pbar.close()
+    
+    # save infos and rewards array
+    np.save(CONFIGS.infos_path / f'a2c_{CONFIGS.experiment_name}_infos.npy', infos)
+    np.save(CONFIGS.rewards_path / f'a2c_{CONFIGS.experiment_name}_rewards.npy', mean_rewards)
     print(mean_rewards)
-    np.save('reinforce_mean_rewards.npy', mean_rewards)
     
-    plt.figure(figsize=(12, 6))
-    plt.plot(mean_rewards)
-    plt.xlabel('Episodes')
-    plt.ylabel('Mean Reward')
-    plt.title('REINFORCE mean rewards')
-    plt.grid()
-    plt.ylim(-3, 5)
-    plt.savefig('a2c_mean_rewards.png')
-    
-    # print(fertilizer_reserve)
 if __name__ == "__main__":
     main()
 
